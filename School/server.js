a6dadf8a2dfef4bd2b09b6ed67001368daee203c
vajra <kowtilyavajra@gmail.com>
2025-05-01 17:37:39 +0530

    new codes

--- School/server.js
+++ School/server.js
@@ -25,11 +25,6 @@ const mongoOptions = {
     serverSelectionTimeoutMS: 5000,
     socketTimeoutMS: 45000,
     family: 4,
-    ssl: true,
-    sslValidate: true,
-    sslCA: undefined, // Let MongoDB driver handle the CA
-    sslCert: undefined,
-    sslKey: undefined,
     tls: true,
     tlsAllowInvalidCertificates: false,
     tlsAllowInvalidHostnames: false,
